743f6ce519a32fc592d77110e864bf35d3b19985
Vedran Hernaus <>
2024-05-06 21:29:01 +0200

    initial setup

---
+++ Cocktails/Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Vedran Hernaus on 06.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
